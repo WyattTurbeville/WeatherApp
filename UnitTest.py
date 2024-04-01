038ecd897fc071c668350046f7f6006510543ad4
Wyatt.Turbeville <you@example.com>
2024-04-01 00:13:27 -0500

    Comitting my branch

---
+++ UnitTest.py
@@ -0,0 +1,6 @@
+import inc_dec    # The code to test
+import unittest   # The test framework
+
+class Test_TestIncrementDecrement(unittest.TestCase):
+    def test_increment(self):
+        self.assertEqual(inc_dec.increment(3), 4)
