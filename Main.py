d341680afbc96da5b9af49cf6a600d0facd8b68e
Wyatt.Turbeville <you@example.com>
2024-04-07 18:58:06 -0500

    still working on this

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
-print("Hello World")
-print("Test 1")
-print("This is Lyndon")
-print("This is Wyatt")
+import requests
+import json
+
+response = requests.get("https://api.weather.gov/gridpoints/TOP/31,80/forecast")
+data = response.json()
+print(data['properties']['periods'][0])
